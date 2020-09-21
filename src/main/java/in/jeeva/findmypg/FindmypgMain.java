a9f16ff843a100f15bce79261f57fe35c152f315
Jeevanantham-N <jevvjeeva001@gmail.com>
2020-09-21 09:37:05 +0530

    make it better

--- src/main/java/in/jeeva/findmypg/FindmypgMain.java
+++ src/main/java/in/jeeva/findmypg/FindmypgMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class findmypg {
+public class FindmypgMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(findmypg.class, args);
+		SpringApplication.run(FindmypgMain.class, args);
 	}
 
 }
